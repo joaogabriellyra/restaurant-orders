80ce59dbfa3bf7c2f0beb6f2b5c035e84631aa7a
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-26 20:26:09 -0300

    requisito 3 concluído

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
+from csv import DictReader
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, encoding='utf-8') as file:
+            menu = list(DictReader(file, delimiter=","))
+
+        self.dishes = set()
+        self.dishes_names = {}
+
+        for snack in menu:
+            plate = snack["dish"]
+            if plate not in self.dishes_names:
+                dish = Dish(plate, float(snack['price']))
+                self.dishes.add(dish)
+                self.dishes_names[plate] = dish
+            else:
+                dish = self.dishes_names[plate]
+            dish.add_ingredient_dependency(
+                Ingredient(snack["ingredient"]),
+                int(snack["recipe_amount"])
+            )
